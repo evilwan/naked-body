93f406a1ca0a3a616f372c21cfb136c4117c77e7
Eddy Vanlerberghe <evilwan@gmail.com>
2021-12-17 16:18:12 +0100

    Changed year in license text.

--- src/main/java/evilwan/nakedbody/StdioEncapsulation.java
+++ src/main/java/evilwan/nakedbody/StdioEncapsulation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Eddy Vanlerberghe.  All rights reserved.
+ * Copyright (c) 2021 Eddy Vanlerberghe.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
