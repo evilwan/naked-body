77d5ea64f052dfe597caf446177b4547d56f86d4
Eddy Vanlerberghe <evilwan@gmail.com>
2021-12-17 16:22:34 +0100

    Changed year in license text. Rewrote logic for output file extension using content-type header.

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Eddy Vanlerberghe.  All rights reserved.
+ * Copyright (c) 2021 Eddy Vanlerberghe.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,8 +34,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 import java.util.List;
 import evilwan.nakedbody.*;
 
@@ -64,6 +63,53 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, BodySav
      */
     private final static String SAVE_DIALOG_TITLE = "Save naked body";
     /**
+     * Common mime-types and their corresponding file extension
+     * <p>
+     * Note that the mime type strings below are patterns to be searched for
+     * in the actual content-type headers. E.g. "/xml" covers several variations
+     * for XML that all include this substring.
+     */
+    private final static Map<String, String> MIME2TYPE =
+        Map.ofEntries(
+                      new AbstractMap.SimpleEntry<String, String>("text/plain", ".txt"),
+                      new AbstractMap.SimpleEntry<String, String>("text/html", ".html"),
+                      new AbstractMap.SimpleEntry<String, String>("text/css", ".css"),
+                      new AbstractMap.SimpleEntry<String, String>("javascript", ".js"),
+                      new AbstractMap.SimpleEntry<String, String>("woff2", ".woff2"),
+                      new AbstractMap.SimpleEntry<String, String>("font/ttf", ".ttf"),
+                      new AbstractMap.SimpleEntry<String, String>("image/gif", ".gif"),
+                      new AbstractMap.SimpleEntry<String, String>("image/png", ".png"),
+                      new AbstractMap.SimpleEntry<String, String>("image/jpeg", ".jpg"),
+                      new AbstractMap.SimpleEntry<String, String>("image/x-icon", ".ico"),
+                      new AbstractMap.SimpleEntry<String, String>("image/svg", ".svg"),
+                      new AbstractMap.SimpleEntry<String, String>("application/zip", ".zip"),
+                      new AbstractMap.SimpleEntry<String, String>("font-woff", ".woff"),
+                      new AbstractMap.SimpleEntry<String, String>("application/pdf", ".pdf"),
+                      new AbstractMap.SimpleEntry<String, String>("image/webp", ".webp"),
+                      new AbstractMap.SimpleEntry<String, String>("application/x-pem-file", ".pem"),
+                      new AbstractMap.SimpleEntry<String, String>("video/mp4", ".mp4"),
+                      new AbstractMap.SimpleEntry<String, String>("video/mpeg", ".mpg"),
+                      new AbstractMap.SimpleEntry<String, String>("video/webm", ".webm"),
+                      new AbstractMap.SimpleEntry<String, String>("video/x-msvideo", ".avi"),
+                      new AbstractMap.SimpleEntry<String, String>("image/bmp", ".bmp"),
+                      new AbstractMap.SimpleEntry<String, String>("application/x-bzip", ".bz"),
+                      new AbstractMap.SimpleEntry<String, String>("application/x-bzip2", ".bz2"),
+                      new AbstractMap.SimpleEntry<String, String>("text/csv", ".csv"),
+                      new AbstractMap.SimpleEntry<String, String>("application/epub+zip", ".epub"),
+                      new AbstractMap.SimpleEntry<String, String>("application/gzip", ".gz"),
+                      new AbstractMap.SimpleEntry<String, String>("audio/mpeg", ".mp3"),
+                      new AbstractMap.SimpleEntry<String, String>("/xml", ".xml"),
+                      new AbstractMap.SimpleEntry<String, String>("json", ".json")
+                      );
+    /**
+     * Prefix to use for output files.
+     */
+    private final static String OUTFILE_PREFIX = (new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-SSS")).format(new Date());;
+    /**
+     * Count to use for output files.
+     */
+    private static int _outcount = 0;
+    /**
      * Main set of Burp callback methods
      */
     private IBurpExtenderCallbacks _callbacks;
@@ -87,7 +133,7 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, BodySav
      * IO stream do dump error messages on
      */
     private PrintStream _stderr = null;
-
+    
     /**
      * Main entry point for Burp extension.
      * <p>
@@ -155,43 +201,36 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, BodySav
      * @param save_resp <code>true</code> if response bodies have to be saved
      */
     public void saveBodies(String directory, boolean save_req, boolean save_resp) {
-        //_stdout.println("saveBodies() -- Number of selected items:" + _them_proxy_lines.length +
-        //        ", save_req=" + save_req + ", save_resp=" + save_resp + ", directory=" + directory);
         //
         // Loop over all selected items in the proxy request/response list
         //
-        String prefix = (new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")).format(new Date());
-        int i = 0;
-
         for(IHttpRequestResponse reqresp : _them_proxy_lines) {
-            i += 1;
-            String filnamreq =  String.format("%s/%s-req-%d.dat", directory, prefix, i);
-            String filnamresp =  String.format("%s/%s-resp-%d.dat", directory, prefix, i);
-            while((new File(filnamreq)).exists() || (new File(filnamresp)).exists()) {
-                i += 1;
-                filnamreq =  String.format("%s/%s-req-%d.dat", directory, prefix, i);
-                filnamresp =  String.format("%s/%s-resp-%d.dat", directory, prefix, i);
-            }
+            _outcount += 1;
             if(save_req) {
                 //_stdout.println("---- saving request for item: " + i);
-                //
-                // We need to save the request
                 //
                 // Get request info object
                 //
                 byte[] bar = reqresp.getRequest();
                 IRequestInfo info = _helpers.analyzeRequest(bar);
                 int body_ofs = info.getBodyOffset();
-                if (body_ofs < bar.length) {
-                    if(info.getContentType() == IRequestInfo.CONTENT_TYPE_URL_ENCODED) {
-                        byte[] tmpbar = new byte[bar.length - body_ofs];
-                        System.arraycopy(bar, body_ofs, tmpbar, 0, tmpbar.length);
-                        byte[] plain = _helpers.urlDecode(tmpbar);
-                        dumpFile(filnamreq, plain, 0, plain.length);
-                    } else {
-                        dumpFile(filnamreq, bar, body_ofs, bar.length - body_ofs);
-                    }
-                }
+               if (body_ofs < bar.length) {
+                   //
+                   // We need to save the request: provide some halfway useful name for
+                   // the output file (if possible)
+                   //
+                   String filnamreq =  String.format("%s/%s-req-%06d%s",
+                                                     directory, OUTFILE_PREFIX, _outcount,
+                                                     guessFileType(info.getHeaders(), bar, body_ofs));
+                   if(info.getContentType() == IRequestInfo.CONTENT_TYPE_URL_ENCODED) {
+                       byte[] tmpbar = new byte[bar.length - body_ofs];
+                       System.arraycopy(bar, body_ofs, tmpbar, 0, tmpbar.length);
+                       byte[] plain = _helpers.urlDecode(tmpbar);
+                       dumpFile(filnamreq, plain, 0, plain.length);
+                   } else {
+                       dumpFile(filnamreq, bar, body_ofs, bar.length - body_ofs);
+                   }
+               }
             }
             if(save_resp) {
                 //
@@ -202,14 +241,69 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, BodySav
                 //
                 byte[] bar = reqresp.getResponse();
                 IResponseInfo info = _helpers.analyzeResponse(bar);
+                //
+                // Try to provide semi-intelligent guess as to which filetype to use
+                //
+                // Note that Burp does not give the actual content-type header (if present)
+                //
+                // The inferred mimetype is not of much use, unfortunately (that is: not more
+                // than the stated mimetype)
+                //
+                //_stdout.println("---- response: mimetype=" + mimetype + ", filename=" + filnamresp);
                 int body_ofs = info.getBodyOffset();
                 if (body_ofs < bar.length) {
+                    String filnamresp =  String.format("%s/%s-resp-%06d%s",
+                                                     directory, OUTFILE_PREFIX, _outcount,
+                                                     guessFileType(info.getHeaders(), bar, body_ofs));
                     dumpFile(filnamresp, bar, body_ofs, bar.length - body_ofs);
                 }
             }
         }
+    }
+    /**
+     * Analyze set of request/response headers and try to determine the
+     * nature of the data being sent/received.
+     *
+     * @param hdrs set of request/response headers as retrieved from Burp
+     * @return file extension that best matches the available information from the headers
+     */
+    private String guessFileType(List<String> hdrs, byte[] body, int ofs) {
+        //say("guessFileType() -- entered.");
+        String out = null;
         //
-        // Check if we need to
+        // Plan A: try analysis of actual body bytes
+        //
+        if((body.length > 4) &&
+           (body[ofs] == 0x00) &&
+           (body[ofs + 1] == (byte) 'a') &&
+           (body[ofs + 2] == (byte) 's') &&
+           (body[ofs + 3] == (byte) 'm')) {
+            //say("guessFileType() -- wasm detected.");
+            return ".wasm";
+        }
+        //
+        // Plan B: no type found based on body content, try mime type header
+        //
+        for(String line: hdrs) {
+            String hdr = line.trim().toLowerCase();
+            if(hdr.startsWith("content-type")) {
+                //
+                // Gotcha: provide reasonable set of checks
+                //
+                //say("guessFileType() -- content-type header: " + hdr);
+                for(Map.Entry<String,String> e: MIME2TYPE.entrySet()) {
+                    if(hdr.contains(e.getKey())) {
+                        //say("guessFileType() -- returning: " + e.getValue());
+                        return e.getValue();
+                    }
+                }
+            }
+        }
+        //
+        // Last chance: provide default ".dat" extension
+        //
+        //say("guessFileType() -- nothing found. returning: .dat");
+        return ".dat";
     }
 
     /**
@@ -264,5 +358,15 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, BodySav
     @Override
     public PrintStream getStderr() {
         return _stderr;
+    }
+    /**
+     * Helper method for printing messages.
+     * <p>
+     * The printed message is written to <code>System.out</code> and starts with the current
+     * class name, followed by two dashes, followed by the specified text.
+     * @param s contains the <code>String</code> value to print.
+     */
+    private void say(String s) {
+        _stdout.println("BurpExtender -- " + s);
     }
 }
